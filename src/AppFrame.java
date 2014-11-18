7a7d8a507471899c07c3b129e70e155421d4b9ca
Mark Bogdan <bogdmark@gmail.com>
2014-11-18 14:33:58 +0100

    AppFrame

---
+++ src/AppFrame.java
@@ -0,0 +1,151 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Marci on 2014.11.18..
+ */
+public class AppFrame extends JFrame{
+
+    JTextField actorFolderField = new JTextField(20);
+    JTextField movieFolderField = new JTextField(20);
+    JLabel actorFolderLabel = new JLabel("Actors' folder:");
+    JLabel movieFolderLabel = new JLabel("Movies' folder:");
+    JButton actorBrowseButton = new JButton("...");
+    JButton movieBrowseButton = new JButton("...");
+    JTextField actorName1Field = new JTextField(20);
+    JTextField actorName2Field = new JTextField(20);
+    JButton searchButton = new JButton("Search");
+    JButton detailsButton = new JButton("Details");
+    JButton prevButton = new JButton("Prev");
+    JButton nextButton = new JButton("Next");
+    JTextArea hitsArea = new JTextArea();
+    JLabel actorNamesLabel = new JLabel("Actors/Actresses:");
+    JLabel hitsLabel = new JLabel("Hit(s):");
+
+
+    private void enter()
+    {
+        JPanel actorBrowsePanel = new JPanel();
+        JPanel movieBrowsePanel = new JPanel();
+        actorBrowsePanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        movieBrowsePanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        actorBrowsePanel.add(actorFolderLabel);
+        actorBrowsePanel.add(actorFolderField);
+        actorBrowsePanel.add(actorBrowseButton);
+        movieBrowsePanel.add(movieFolderLabel);
+        movieBrowsePanel.add(movieFolderField);
+        movieBrowsePanel.add(movieBrowseButton);
+
+        JPanel searchPanel = new JPanel();
+        searchPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        searchPanel.add(actorNamesLabel);
+        searchPanel.add(actorName1Field);
+        searchPanel.add(actorName2Field);
+        searchPanel.add(searchButton);
+
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new BorderLayout());
+        topPanel.add(actorBrowsePanel, BorderLayout.PAGE_START);
+        topPanel.add(movieBrowsePanel, BorderLayout.CENTER);
+        topPanel.add(searchPanel, BorderLayout.PAGE_END);
+
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        bottomPanel.add(prevButton);
+        bottomPanel.add(nextButton);
+        bottomPanel.add(detailsButton);
+
+/*
+        hitsArea.setMaximumSize(new Dimension(300, 300));
+        hitsArea.setPreferredSize(new Dimension(300, 300));
+*/
+        hitsArea.setEditable(false);
+        hitsArea.setAutoscrolls(true);
+
+        JPanel hitsPanel = new JPanel();
+        hitsPanel.setLayout(new BorderLayout());
+        hitsPanel.add(hitsLabel, BorderLayout.PAGE_START);
+        hitsPanel.add(hitsArea, BorderLayout.CEN 
+
+        this.setLayout(new BorderLayout());
+        this.add(topPanel, BorderLayout.PAGE_STA 
+        this.add(hitsPanel, BorderLayout.CENTER) 
+        this.add(bottomPanel, BorderLayout.PAGE_ 
+    }
+
+    private void buttonListeners()
+    {
+        actorBrowseButton.setActionCommand("acto 
+        ActionListener a1 = new ButtonActionList 
+        actorBrowseButton.addActionListener(a1);
+
+        movieBrowseButton.setActionCommand("movi 
+        ActionListener a2 = new ButtonActionList 
+        actorBrowseButton.addActionListener(a2);
+
+        searchButton.setActionCommand("search");
+        ActionListener a3 = new ButtonActionList 
+        actorBrowseButton.addActionListener(a3);
+
+        prevButton.setActionCommand("prev");
+        ActionListener a4 = new ButtonActionList 
+        actorBrowseButton.addActionListener(a4);
+
+        nextButton.setActionCommand("next");
+        ActionListener a5 = new ButtonActionList 
+        actorBrowseButton.addActionListener(a5);
+
+        detailsButton.setActionCommand("details" 
+        ActionListener a6 = new ButtonActionList 
+        actorBrowseButton.addActionListener(a6);
+    }
+
+    public class ButtonActionListener implements 
+
+        @Override
+        public void actionPerformed(ActionEvent  
+
+            if (e.getActionCommand().equals("act 
+            {
+                actorFolderField.setText("D:\\") 
+            }
+            if (e.getActionCommand().equals("mov 
+            {
+
+            }
+            if (e.getActionCommand().equals("sea 
+            {
+
+            }
+            if (e.getActionCommand().equals("pre 
+            {
+
+            }
+            if (e.getActionCommand().equals("nex 
+            {
+
+            }
+            if (e.getActionCommand().equals("det 
+            {
+
+            }
+
+        }
+    }
+    public AppFrame()
+    {
+        super("Movie Hunter");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setMinimumSize(new Dimension(700, 500));
+        enter();
+        buttonListeners();
+    }
+
+    public static void main(String args[])
+    {
+        AppFrame af = new AppFrame();
+        af.setVisible(true);
+    }
+}
